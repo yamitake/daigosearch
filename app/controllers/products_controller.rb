9c61dfddfb803ccd2659f45fa1d2fec0bd1fa86f
yamitake <take.yapr@gmail.com>
2020-01-08 17:43:13 +0900

    検索できる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,11 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = if params[:q].blank?
+                  Product.all
+                else
+                  Product.search(params[:q])
+                end
   end
 
   # GET /products/1
